9ae64cdc79c92aa927f02659e2d428348f74e84f
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2025-01-13 15:23:57 +0000

    Revert "added POST assistance endpoint"
    This reverts commit ba39d95f50c863305f301c88e84c6ac2c774e406.

--- app/main.py
+++ app/main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI, status
-from pydantic import BaseModel
-from enum import Enum
 
 app = FastAPI()
 
-class AssistanceRequest(BaseModel):
-    topic: str
-    description: str
-
 @app.get("/heartbeat", status_code=status.HTTP_204_NO_CONTENT)
 async def heartbeat():
-    return None
-
-@app.post("/assistance", status_code=status.HTTP_204_NO_CONTENT)
-async def request_assistance(request: AssistanceRequest):
     return None
