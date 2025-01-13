81f76a3e1ce7d48f2b270ec77cd8841c07b574f3
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2025-01-13 16:50:57 +0000

    playing with DI

--- app/routes/assistance.py
+++ app/routes/assistance.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, status
+import typing
+from fastapi import APIRouter, Depends, status
 from pydantic import BaseModel
 
 router = APIRouter()
@@ -7,6 +8,23 @@ class AssistanceNotification(BaseModel):
     topic: str
     description: str
 
+class AssistanceRequest(BaseModel):
+    description: str
+
+class Channel(typing.Protocol):
+    def send(self, request: AssistanceRequest):
+        raise NotImplementedError()
+
+class ChannelResolver(typing.Protocol):
+    def get(self, topic: str) -> Channel:
+        raise NotImplementedError()
+
+def get_channel_resolver() -> ChannelResolver:
+    return ChannelResolver()
+
 @router.post("/assistance", status_code=status.HTTP_201_CREATED)
-async def create_assistance_notification(notification: AssistanceNotification):
-    return None
+async def create_assistance_notification(notification: AssistanceNotification, channel_resolver: typing.Annotated[ChannelResolver, Depends(get_channel_resolver)]):
+    channel = channel_resolver.get(notification.topic)
+    channel.send(AssistanceRequest(description=notification.description))
+    return channel
+    
--- app/routes/assistance_test.py
+++ app/routes/assistance_test.py
@@ -1,9 +1,32 @@
+from app.routes.assistance import AssistanceRequest, Channel, get_channel_resolver
 from fastapi.testclient import TestClient
 from app.main import app
 
-client = TestClient(app)
+
+class InMemoryChannel(Channel):
+    def __init__(self):
+        self.notifications = []
+
+    def send(self, notification: AssistanceRequest):
+        self.notifications.append(notification)
+
+class InMemoryChannelResolver:
+    def __init__(self):
+        self.channels = {}
+
+    def add(self, topic: str, channel: Channel):
+        self.channels[topic] = channel   
+
+    def get(self, topic: str) -> Channel:
+        return self.channels[topic]
 
 def test_create_assistance_notification():
+    sales = InMemoryChannel()
+    channel_resolver = InMemoryChannelResolver()
+    channel_resolver.add("Sales", sales)
+    app.dependency_overrides[get_channel_resolver] = lambda: channel_resolver
+    client = TestClient(app)
+    
     notification_data = {
         "topic": "Sales",
         "description": "I need help with my order #12345"
@@ -11,5 +34,6 @@ def test_create_assistance_notification():
     
     response = client.post("/assistance", json=notification_data)
     assert response.status_code == 201
+    assert sales.notifications == [AssistanceRequest(description="I need help with my order #12345")]
 
 # TODO test for invalid body
