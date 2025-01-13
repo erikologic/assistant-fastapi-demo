ba39d95f50c863305f301c88e84c6ac2c774e406
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2025-01-13 15:22:11 +0000

    added POST assistance endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, status
+from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
 
+class AssistanceRequest(BaseModel):
+    topic: str
+    description: str
+
 @app.get("/heartbeat", status_code=status.HTTP_204_NO_CONTENT)
 async def heartbeat():
+    return None
+
+@app.post("/assistance", status_code=status.HTTP_204_NO_CONTENT)
+async def request_assistance(request: AssistanceRequest):
     return None
