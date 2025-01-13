1c09c647a5e053a4672df1440f63553933939140
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2025-01-13 17:09:35 +0000

    added test for pricing

--- app/routes/assistance_test.py
+++ app/routes/assistance_test.py
@@ -32,4 +32,24 @@ def test_create_sales_assistance_notification():
     ]
 
 
+def test_create_pricing_assistance_notification():
+    mocked_channels = {
+        "Sales": InMemoryChannel(),
+        "Pricing": InMemoryChannel(),
+    }
+    app.dependency_overrides[channels] = lambda: mocked_channels
+    client = TestClient(app)
+
+    notification_data = {
+        "topic": "Pricing",
+        "description": "What's the price for the product #12345?",
+    }
+
+    response = client.post("/assistance", json=notification_data)
+    assert response.status_code == 201
+    assert mocked_channels["Pricing"].notifications == [
+        Notification(description="What's the price for the product #12345?")
+    ]
+
+
 # TODO test for invalid body
