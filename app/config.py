7c738937be0be9039ceba9840aac26e262082ae2
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2025-01-14 21:06:52 +0000

    fix Config warning

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
-
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 
 
 class Settings(BaseSettings):
@@ -10,9 +10,7 @@ class Settings(BaseSettings):
     auth0_algorithms: str
     slack_token: str
 
-    class Config:
-        env_file = ".env"
-        extra = "allow"
+    model_config = ConfigDict(env_file=".env", extra="allow")
 
 
 @lru_cache()
