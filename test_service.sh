156fb123506cb444762113f28de4ba7487bb059f
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2025-01-14 22:26:01 +0000

    parametirise test_service

--- test_service.sh
+++ test_service.sh
@@ -1,10 +1,23 @@
 #! /bin/sh
 
-## This script is used as an example to generate an authorised API request to the API
+## This script is used to generate authorised requests to the API
+## Usage: 
+##   ./test_service.sh -e assistance  # calls assistance endpoint (requires auth)
+##   ./test_service.sh -e heartbeat   # calls heartbeat endpoint (no auth)
 
 set -e
-
 . .env
+
+# Default values
+ENDPOINT="heartbeat"
+
+# Parse command line options
+while getopts "e:" opt; do
+    case $opt in
+        e) ENDPOINT=$OPTARG ;;
+        ?) echo "Usage: $0 [-e endpoint]" >&2; exit 1 ;;
+    esac
+done
 
 get_token() {
     curl --request POST \
@@ -13,12 +26,25 @@ get_token() {
     --data grant_type=client_credentials \
     --data audience="$AUTH0_API_AUDIENCE" \
     --data client_id="$TEST_CLIENT_ID" \
-    --data client_secret="$TEST_CLIENT_SECRET"  | jq '.access_token' | xargs echo -n
-   }
+    --data client_secret="$TEST_CLIENT_SECRET" | jq '.access_token' | xargs echo -n
+}
 
-TOKEN=$(get_token)
+case $ENDPOINT in
+    "assistance")
+        TOKEN=$(get_token)
+        curl -i -X POST "http://127.0.0.1:8000/assistance/" \
+            -H "Content-Type: application/json" \
+            -d '{"topic": "Sales", "description": "I need help so much!"}' \
+            -H "Authorization: Bearer ${TOKEN}"
+        ;;
+    "heartbeat")
+        curl -i -X GET "http://127.0.0.1:8000/heartbeat" \
+            -H "Content-Type: application/json"
+        ;;
+    *)
+        echo "Unknown endpoint: $ENDPOINT"
+        echo "Valid endpoints are: assistance, heartbeat"
+        exit 1
+        ;;
+esac 
 
-curl -i -X POST "http://127.0.0.1:8000/assistance/" \
-    -H "Content-Type: application/json" \
-    -d '{"topic": "Sales", "description": "I need help so much!"}' \
-    -H "Authorization: Bearer ${TOKEN}" 
